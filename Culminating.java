a2f5df665f5569a0648bdc5201de08181c4158c1
Benjamin Kim <benjaminsangminkim@gmail.com>
2021-05-27 21:00:34 -0400

    Some new features

--- Culminating.java
+++ Culminating.java
@@ -1,13 +1,13 @@
+import java.io.*;
+import java.util.*;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Culminating extends Application implements EventHandler<ActionEvent>{
@@ -15,15 +15,17 @@ public class Culminating extends Application implements EventHandler<ActionEvent
     Stage window;
     Scene scene1, scene2, scene3, scene4;
     Button button, goBack1, goBack2, goBack3, edit1, edit2, edit3, use1, use2, use3;
-    Button[] goBack, edit, use;
     Label[] label8;
+    File math = new File("Math.txt");
+    File chem = new File("Chemistry.txt");
+    File physics = new File("Physics.txt");
 
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
+    public void start(Stage primaryStage) throws IOException {
         window = primaryStage;
         window.setTitle("Question Bank");
 
@@ -40,22 +42,20 @@ public class Culminating extends Application implements EventHandler<ActionEvent
         Label label6 = new Label("Welcome to the Chemistry files!");
         Label label7 = new Label("Welcome to the Physics files!");
 
-        /*
-        Label label801 = new Label("Would you like to import or export a question?");
-        label801.setTranslateY(20);
-        Label label802 = new Label("Would you like to import or export a question?");
-        label802.setTranslateY(20);
-        Label label803 = new Label("Would you like to import or export a question?");
-        label803.setTranslateY(20);
-        */
         // Label that has a question
         label8 = new Label[3];
         for (int i = 0; i < 3; i++) {
-            label8[i] = new Label("Would you like to import or export a question?");
+            label8[i] = new Label("Would you like to make or use a question?");
             label8[i].setTranslateY(20);
         }
 
-        // A continue button for scene 1
+        // Drop down menu
+        ComboBox<String> comboBox = new ComboBox<>();
+        comboBox.getItems().addAll("Math", "Chemistry", "Physics");
+        comboBox.setPromptText("Subjects");
+        comboBox.setTranslateY(10);
+
+        // A continue button for scene 1 after the user selects from the drop down menu
         button = new Button("Continue");
         button.setTranslateY(50);
 
@@ -69,46 +69,28 @@ public class Culminating extends Application implements EventHandler<ActionEvent
         goBack3 = new Button("Go Back");
         goBack3.setTranslateX(-160);
         goBack3.setTranslateY(130);
-        /*
-        goBack = new Button[3];
-        for (int i = 0; i < 3; i++) {
-            goBack[i] = new Button("Go Back");
-            goBack[i].setTranslateX(-160);
-            goBack[i].setTranslateY(130);
-        }
-        */
-
-
-        edit = new Button[3];
-        for (int i = 0; i < 3; i++) {
-            edit[i] = new Button("Import");
-            edit[i].setTranslateX(-40);
-            edit[i].setTranslateY(50);
-        }
-        /*
-        use = new Button[3];
-        for (int i = 0; i < 3; i++) {
-            use[i] = new Button("Export");
-            use[i].setTranslateX(40);
-            use[i].setTranslateY(50);
-        }
-        */
         
-        use1 = new Button("Export");
-        use1.setTranslateX(40);
-        use1.setTranslateY(50);
-        use2 = new Button("Export");
-        use2.setTranslateX(40);
-        use2.setTranslateY(50);
-        use3 = new Button("Export");
-        use3.setTranslateX(40);
-        use3.setTranslateY(50);
+        // Buttons that send users to make their own questions
+        edit1 = new Button("Make Math Questions!");
+        edit1.setTranslateX(-75);
+        edit1.setTranslateY(50);
+        edit2 = new Button("Make Chemistry Questions!");
+        edit2.setTranslateX(-80);
+        edit2.setTranslateY(50);
+        edit3 = new Button("Make Physics Questions!");
+        edit3.setTranslateX(-80);
+        edit3.setTranslateY(50);
 
-        // Drop down menu
-        ComboBox<String> comboBox = new ComboBox<>();
-        comboBox.getItems().addAll("Math", "Chemistry", "Physics");
-        comboBox.setPromptText("Subjects");
-        comboBox.setTranslateY(10);
+        // Buttons that sends the user to copy and paste existing questions
+        use1 = new Button("Use Math Questions!");
+        use1.setTranslateX(80);
+        use1.setTranslateY(50);
+        use2 = new Button("Use Chemistry Questions!");
+        use2.setTranslateX(80);
+        use2.setTranslateY(50);
+        use3 = new Button("Use Physics Questions!");
+        use3.setTranslateX(75);
+        use3.setTranslateY(50);
 
         // The introduction scene
         StackPane layout1 = new StackPane();
@@ -117,17 +99,17 @@ public class Culminating extends Application implements EventHandler<ActionEvent
 
         // Math Scene
         StackPane layout2 = new StackPane();
-        layout2.getChildren().addAll(label5, goBack1, label8[0], use1, edit[0]);
+        layout2.getChildren().addAll(label5, goBack1, label8[0], use1, edit1);
         scene2 = new Scene(layout2, 400, 300);
 
         // Chemistry Scene
         StackPane layout3 = new StackPane();
-        layout3.getChildren().addAll(label6, goBack2, label8[1], use2, edit[1]);
+        layout3.getChildren().addAll(label6, goBack2, label8[1], use2, edit2);
         scene3 = new Scene(layout3, 400, 300);
 
         // Physics Scene
         StackPane layout4 = new StackPane();
-        layout4.getChildren().addAll(label7, goBack3, label8[2], use3, edit[2]);
+        layout4.getChildren().addAll(label7, goBack3, label8[2], use3, edit3);
         scene4 = new Scene(layout4, 400, 300);
 
         // Starts with scene 1
@@ -139,29 +121,63 @@ public class Culminating extends Application implements EventHandler<ActionEvent
         goBack1.setOnAction(e -> window.setScene(scene1));
         goBack2.setOnAction(e -> window.setScene(scene1));
         goBack3.setOnAction(e -> window.setScene(scene1));
-        use1.setOnAction(e -> sendToFile());
-    }
 
+        // When the user presses one of the use buttons, the program opens the notepad and the user can copy and paste from there.
+        use1.setOnAction(e -> {
+            try {
+                sendToFile(math);
+            } catch (IOException e1) {
+            }
+        });
+        use2.setOnAction(e -> {
+            try {
+                sendToFile(chem);
+            } catch (IOException e1) {
+            }
+        });
+        use3.setOnAction(e -> {
+            try {
+                sendToFile(physics);
+            } catch (IOException e1) {
+            }
+        });
+    }
+    
+    /*
+     * Author - Benjamin Kim
+     * Description - Takes the user's choice from the drop down menu and sends them to that subjects scene.
+     * 
+     * @param - comboBox(Drop down menu), Stage primaryStage
+     * @return - No return
+     */
     public void getChoice(ComboBox<String> comboBox, Stage primaryStage) {
         String subject = comboBox.getValue();
+
         if (subject.equals("Math")) {
             primaryStage.setScene(scene2);
-            System.out.println("Welcome to the math files");
         }
         else if(subject.equals("Chemistry")) {
             primaryStage.setScene(scene3);
-            System.out.println("Welcome to the chemistry files");
         }
         else if(subject.equals("Physics")) {
             primaryStage.setScene(scene4);
-            System.out.println("Welcome to the physics files");
-        }
-        else {
-            System.out.println("Select a valid subject");
         }
     }
 
-    public void sendToFile() {
+    /*
+     * Author - Benjamin Kim
+     * Description - Opens a notepad for the specific subject the user chose
+     * 
+     * @param - File file
+     * @return - No return
+     */
+    public void sendToFile(File file) throws IOException {
+        String fileName = file.getName();
+        ProcessBuilder pb = new ProcessBuilder("Notepad.exe", fileName);
+        pb.start();
+    }
+
+    public void makeQuestions(File file) throws IOException {
         
     }
 
